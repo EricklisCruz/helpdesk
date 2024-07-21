ad9c4fca48a9fa2de086d9ddac9fad88d1314ebc
EricklisCruz <erickliscruz@gmail.com>
2024-07-21 16:05:55 -0300

    JPA annotation domain classes

--- src/main/java/com/springestudos/helpdesk/domain/Cliente.java
+++ src/main/java/com/springestudos/helpdesk/domain/Cliente.java
@@ -1,17 +1,30 @@
 package com.springestudos.helpdesk.domain;
 
+import com.springestudos.helpdesk.domain.enums.Perfil;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Cliente extends Pessoa {
+@Entity
+public class Cliente extends Pessoa implements Serializable {
 
+    @Serial
+    private static  final long serialVersionUID = 1L;
+
+    @OneToMany(mappedBy = "cliente")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Cliente() {
+        addPerfis(Perfil.CLIENTE);
     }
 
     public Cliente(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfis(Perfil.CLIENTE);
     }
 
     public List<Chamado> getChamados() {
--- src/main/java/com/springestudos/helpdesk/domain/Tecnico.java
+++ src/main/java/com/springestudos/helpdesk/domain/Tecnico.java
@@ -1,17 +1,30 @@
 package com.springestudos.helpdesk.domain;
 
+import com.springestudos.helpdesk.domain.enums.Perfil;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Tecnico extends Pessoa {
+@Entity
+public class Tecnico extends Pessoa implements Serializable {
 
+    @Serial
+    private static  final long serialVersionUID = 1L;
+
+    @OneToMany(mappedBy = "tecnico")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Tecnico() {
+        addPerfis(Perfil.CLIENTE);
     }
 
     public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfis(Perfil.CLIENTE);
     }
 
     public List<Chamado> getChamados() {
